02fefb3d9968f8c91a66dc9d47ab7c00dca23233
Sven Ziegler <43136984+Svenum@users.noreply.github.com>
2025-05-17 16:40:47 +0200

    add aarch64 support (#25)
    * split formatting and build check in two seperate actions
    
    * add cross compiling
    
    * add multiarch image

--- nix/package/iglu-cache-docker/default.nix
+++ nix/package/iglu-cache-docker/default.nix
@@ -1,11 +1,15 @@
 { dockerTools
 , iglu
 , bash
+, stdenv
 }:
 
+let
+  archType = if (stdenv.hostPlatform.system == "x86_64-linux") then "amd64" else "arm64";
+in
 dockerTools.buildImage {
   name = "iglu-cache-docker";
-  tag = "v${iglu.iglu-cache.version}";
+  tag = "v${iglu.iglu-cache.version}-${archType}";
 
   copyToRoot = [
     iglu.iglu-cache
