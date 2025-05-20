9e4d7002c7079a393cf8996adf3c3a7a3b710e5f
SirBerg <benno@boerg.co>
2025-05-20 20:24:03 +0200

    feat(DB): Moved API Keys to use uuids instead of jwt tokens (#36)

--- utils/apiKeys.ts
+++ utils/apiKeys.ts
@@ -1,6 +1,7 @@
-import jwt from 'jsonwebtoken';
 import 'dotenv/config';
+import { randomUUIDv7 } from 'bun'
 export function makeApiKey(cacheName:string):string{
-    //We can be sure that this exists as we check for it in the index.ts file
-    return jwt.sign({name:cacheName}, process.env.CACHE_JWT_SECRET as string)
+
+    const uid = randomUUIDv7();
+    return `${cacheName}-${uid}`;
 }
