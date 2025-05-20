0626d9dc92d479ae21838e8008f1c082d66fa891
Sven Ziegler <43136984+Svenum@users.noreply.github.com>
2025-05-20 21:37:55 +0200

    move everything to iglu-flake (#37)
    * move everything to iglu-flake
    
    * pin iglu-flake
    
    * remove unneded wrapper

---
+++ nix/pkgs/iglu-cache/default.nix
@@ -0,0 +1,49 @@
+{ buildBunApplication }:
+
+buildBunApplication {
+  src = ../../..;
+
+  nodeModuleHash = "sha256-yLSf11QbDZ09DXlAupAyhNBKDFRt4lA+Z2UH+bxaOrw=";
+
+  extraWrapScript = ''
+    if [ -z "$CACHE_ROOT_DOMAIN" ]; then
+      export CACHE_ROOT_DOMAIN="http://localhost:3000"
+    fi
+
+    if [ -z "$POSTGRES_DB" ]; then
+      export POSTGRES_DB="cache"
+    fi
+
+    if [ -z "$POSTGRES_HOST" ]; then
+      export POSTGRES_HOST="postgres"
+    fi
+
+    if [ -z "$POSTGRES_USER" ]; then
+      export POSTGRES_USER="postgres"
+    fi
+
+    if [ -z "$POSTGRES_PASSWORD" ]; then
+      export POSTGRES_PASSWORD="postgres"
+    fi
+
+    if [ -z "$POSTGRES_PORT" ]; then
+      export POSTGRES_PORT="5432"
+    fi
+
+    if [ -z "$CACHE_FILESYSTEM_DIR" ]; then
+      export CACHE_FILESYSTEM_DIR="/tmp/iglu-cache"
+    fi
+
+    if [ -z "$CACHE_JWT_SECRET" ]; then
+      export CACHE_JWT_SECRET="secret"
+    fi
+  '';
+
+  bunScript = "prod";
+
+  filesToInstall = [
+    "index.ts"
+    "routes"
+    "utils"
+  ];
+}
