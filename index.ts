d5ea0f5b24bd128cc2f50bed1f075dbae4bee97f
Svenum <s.ziegler@holypenguin.net>
2025-11-12 10:45:36 +0100

    fix

--- index.ts
+++ index.ts
@@ -9,6 +9,8 @@ import 'dotenv/config'
 import {migrate} from "./utils/migrations.ts";
 import Logger from "@iglu-sh/logger";
 import { startExporter } from './utils/metric.ts';
+import {start} from 'repl';
+import {exit} from 'process';
 
 const app = require('express')()
 
@@ -62,120 +64,126 @@ console.log("Filesystem Dir:\t" + process.env.CACHE_FILESYSTEM_DIR)
 console.log("Filesystem Max GB:\t" + process.env.CACHE_MAX_GB + ' GB')
 console.log("\n\n\n")
 
-let isReady = false
-let Database: db = new db();
-while(!isReady){
-    try {
-        Database = new db()
-        await Database.connect()
-        isReady = true
-    }
-    catch(e){
-        Logger.error(`Error whilst connecting to Database, is your Server up? Waiting 5 Seconds to retry ${e}`)
-        //Destroy the Database instance to prevent memory leaks and to avoid a bug with reconnecting client
-        await Database.destroy()
-        //Wait for 5 seconds
-        await new Promise((resolve) => setTimeout(resolve, 5000))
-    }
+async function startDB(){
+  let isReady = false
+  let Database: db = new db();
+  while(!isReady){
+      try {
+          Database = new db()
+          await Database.connect()
+          isReady = true
+      }
+      catch(e){
+          Logger.error(`Error whilst connecting to Database, is your Server up? Waiting 5 Seconds to retry ${e}`)
+          //Destroy the Database instance to prevent memory leaks and to avoid a bug with reconnecting client
+          await Database.destroy()
+          //Wait for 5 seconds
+          await new Promise((resolve) => setTimeout(resolve, 5000))
+      }
+  }
+  await Database.setupDB();
+
+  //Insert the default cache if it does not exist
+  await Database.insertServerSettings(
+      process.env.CACHE_FILESYSTEM_DIR!,
+      process.env.LOG_LEVEL,
+      process.env.CACHE_MAX_GB,
+      process.env.CACHE_ROOT_DOMAIN
+  )
+
+  await Database.getAllCaches().then(async (caches:Array<cacheWithKeys>)=>{
+      if(caches.length === 0){
+          //Create a default cache
+          Logger.info('No caches found, creating default cache')
+          await Database.createCache("default", "Read", true, "none", "XZ", `${process.env.CACHE_ROOT_DOMAIN}`)
+      }
+      caches = await Database.getAllCaches()
+      //Run migrations
+      await migrate(Database);
+      for (const cache of caches) {
+          //Updating CACHE_ROOT_DOMAIN if needed
+          if(cache.uri != process.env.CACHE_ROOT_DOMAIN && process.env.AUTO_FIX_CACHE_ROOT_DOMAIN !== "false"){
+            Logger.debug("Updating CACHE_ROOT_DOMAIN for cache \"" + cache.name + "\"")
+            await Database.updateCacheURI(process.env.CACHE_ROOT_DOMAIN, cache.id)
+          }
+          if(cache.allowedKeys.length === 0 || cache.allowedKeys[0] === null || cache.allowedKeys[0] === "NULL"){
+              const cacheKey = makeApiKey(cache.name)
+              Logger.info(`Initial Key for cache ${cache.name}: ${cacheKey}`)
+              try{
+                  let newHash = await Database.appendApiKey(cache.id, cacheKey)
+                  //Add this key to the cache
+                  cache.allowedKeys[0] = newHash
+              }
+              catch(e) {
+                  Logger.error(`Error whilst appending: ${e}`)
+                  return -1
+              }
+          }
+          //Check the public signing keys and "create" a default one
+          if(cache.publicSigningKeys.length === 0 || cache.publicSigningKeys[0] === null){
+              Logger.debug(`No public signing keys found for cache ${cache.name} creating placeholder`)
+              //Get the first api key available in the database
+              const apiKey = cache.allowedKeys[0]
+              if(!apiKey) continue
+
+              //Create a new public signing key
+              await Database.appendPublicKey(cache.id, "<empty>",  apiKey, true)
+          }
+          //Show the public signing key for this cache
+          Logger.debug(`Public signing keys for cache ${cache.name}: ${cache.publicSigningKeys.map((key)=>key == "" ? "<empty>" : key).join(", ")}`)
+      }
+  })
+
+  //Check if there are "dangling" paths in the nar_file directory (i.e paths that are not in the database) and part files
+  const paths = await Database.getDirectAccess().query(`
+      SELECT path, id FROM cache.hashes 
+  `)
+  for(const pathObj of paths.rows){
+      if(!fs.existsSync(pathObj.path)){
+          Logger.debug(`Path ${pathObj.path} does not exist in database, removing from database`)
+          //Delete the path from the database
+          await Database.getDirectAccess().query(`
+              DELETE FROM cache.request WHERE hash = ${pathObj.id}
+          `)
+          await Database.deletePath(pathObj.id)
+
+          continue
+      }
+
+      //Check how big the file is
+      const stats = fs.statSync(pathObj.path);
+      const fileSizeInBytes = stats.size;
+      if(fileSizeInBytes === 0){
+          Logger.debug(`Path ${pathObj.path} is empty, removing from database and unlinking`)
+          //Delete the path from the database
+          await Database.deletePath(pathObj.id)
+          //Unlink the file
+          fs.unlinkSync(pathObj.path)
+      }
+
+  }
+  //Check if there are leftover parts in the nar_file directory
+  for(const cache of await Database.getAllCaches()){
+      const cacheDir = `${process.env.CACHE_FILESYSTEM_DIR}/nar_files/${cache.name}`
+      if(!fs.existsSync(cacheDir)){
+          continue
+      }
+      const files = fs.readdirSync(cacheDir)
+      for(const file of files){
+          //Check if the file is a file that ends with a number instead of .xz or .zstd
+          if(!file.endsWith('.xz') && !file.endsWith('.zstd')){
+              Logger.debug(`Part file ${file} found, removing`)
+              fs.unlinkSync(`${cacheDir}/${file}`)
+          }
+      }
+  }
+
+  await Database.close()
 }
-await Database.setupDB();
 
-//Insert the default cache if it does not exist
-await Database.insertServerSettings(
-    process.env.CACHE_FILESYSTEM_DIR!,
-    process.env.LOG_LEVEL,
-    process.env.CACHE_MAX_GB,
-    process.env.CACHE_ROOT_DOMAIN
-)
-
-await Database.getAllCaches().then(async (caches:Array<cacheWithKeys>)=>{
-    if(caches.length === 0){
-        //Create a default cache
-        Logger.info('No caches found, creating default cache')
-        await Database.createCache("default", "Read", true, "none", "XZ", `${process.env.CACHE_ROOT_DOMAIN}`)
-    }
-    caches = await Database.getAllCaches()
-    //Run migrations
-    await migrate(Database);
-    for (const cache of caches) {
-        //Updating CACHE_ROOT_DOMAIN if needed
-        if(cache.uri != process.env.CACHE_ROOT_DOMAIN && process.env.AUTO_FIX_CACHE_ROOT_DOMAIN !== "false"){
-          Logger.debug("Updating CACHE_ROOT_DOMAIN for cache \"" + cache.name + "\"")
-          await Database.updateCacheURI(process.env.CACHE_ROOT_DOMAIN, cache.id)
-        }
-        if(cache.allowedKeys.length === 0 || cache.allowedKeys[0] === null || cache.allowedKeys[0] === "NULL"){
-            const cacheKey = makeApiKey(cache.name)
-            Logger.info(`Initial Key for cache ${cache.name}: ${cacheKey}`)
-            try{
-                let newHash = await Database.appendApiKey(cache.id, cacheKey)
-                //Add this key to the cache
-                cache.allowedKeys[0] = newHash
-            }
-            catch(e) {
-                Logger.error(`Error whilst appending: ${e}`)
-                return -1
-            }
-        }
-        //Check the public signing keys and "create" a default one
-        if(cache.publicSigningKeys.length === 0 || cache.publicSigningKeys[0] === null){
-            Logger.debug(`No public signing keys found for cache ${cache.name} creating placeholder`)
-            //Get the first api key available in the database
-            const apiKey = cache.allowedKeys[0]
-            if(!apiKey) continue
-
-            //Create a new public signing key
-            await Database.appendPublicKey(cache.id, "<empty>",  apiKey, true)
-        }
-        //Show the public signing key for this cache
-        Logger.debug(`Public signing keys for cache ${cache.name}: ${cache.publicSigningKeys.map((key)=>key == "" ? "<empty>" : key).join(", ")}`)
-    }
-})
-
-//Check if there are "dangling" paths in the nar_file directory (i.e paths that are not in the database) and part files
-const paths = await Database.getDirectAccess().query(`
-    SELECT path, id FROM cache.hashes 
-`)
-for(const pathObj of paths.rows){
-    if(!fs.existsSync(pathObj.path)){
-        Logger.debug(`Path ${pathObj.path} does not exist in database, removing from database`)
-        //Delete the path from the database
-        await Database.getDirectAccess().query(`
-            DELETE FROM cache.request WHERE hash = ${pathObj.id}
-        `)
-        await Database.deletePath(pathObj.id)
-
-        continue
-    }
-
-    //Check how big the file is
-    const stats = fs.statSync(pathObj.path);
-    const fileSizeInBytes = stats.size;
-    if(fileSizeInBytes === 0){
-        Logger.debug(`Path ${pathObj.path} is empty, removing from database and unlinking`)
-        //Delete the path from the database
-        await Database.deletePath(pathObj.id)
-        //Unlink the file
-        fs.unlinkSync(pathObj.path)
-    }
-
+if(process.env.NO_DB !== "true"){
+  await startDB()
 }
-//Check if there are leftover parts in the nar_file directory
-for(const cache of await Database.getAllCaches()){
-    const cacheDir = `${process.env.CACHE_FILESYSTEM_DIR}/nar_files/${cache.name}`
-    if(!fs.existsSync(cacheDir)){
-        continue
-    }
-    const files = fs.readdirSync(cacheDir)
-    for(const file of files){
-        //Check if the file is a file that ends with a number instead of .xz or .zstd
-        if(!file.endsWith('.xz') && !file.endsWith('.zstd')){
-            Logger.debug(`Part file ${file} found, removing`)
-            fs.unlinkSync(`${cacheDir}/${file}`)
-        }
-    }
-}
-
-await Database.close()
 
 if(process.env.PROM_ENABLE === "true"){
   startExporter()
@@ -202,3 +210,16 @@ app.use((req:Request, res:Response) => {
 });
 
 app.listen(3000)
+
+// check if CACHE_ROOT_DOMAIN is set correctly
+try{
+  const test = await fetch(process.env.CACHE_ROOT_DOMAIN + "/api/v1/healthcheck")
+  if(test.status != 200){
+    Logger.error("Invalid CACHE_ROOT_DOMAIN ENV!")
+    exit(1)
+  }
+}catch(e){
+  Logger.error("Invalid 'CACHE_ROOT_DOMAIN' ENV!")
+  exit(1)
+}
+
